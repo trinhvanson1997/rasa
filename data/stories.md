b53c877ae703c809005e6357c10e7fa4b460bb5b
tvson <trinhvanson1997@gmail.com>
2019-09-16 17:13:35 +0700

    add rasa core

---
+++ data/stories.md
@@ -0,0 +1,44 @@
+## cảm ơn
+* cam_on
+    - utter_thankyou
+
+##  chào hỏi bình thường
+* chaohoi1
+    - utter_greet1
+
+## chào hỏi gặp lại
+* chaohoi2
+    - utter_greet2
+    
+## chê 1
+* che1
+    - utter_complain1
+
+## chê 2
+* che2
+    - utter_complain2
+
+## chê 3
+* che3
+    - utter_complain3
+
+## chê 4
+* che4
+    - utter_complain4
+
+## hát hò
+* hat_ho
+    - utter_sing
+
+## hỏi chức năng
+* hoi_chuc_nang
+    - utter_function
+
+## hỏi contact 1
+* hoi_contact1
+    - utter_contact1
+
+## hỏi contact 2
+* hoi_contact2
+    - utter_contact2
+    
--- train_nlu.py
+++ train_nlu.py
@@ -1,23 +1,21 @@
-
-from rasa_nlu.training_data import load_data
-from rasa_nlu.config import RasaNLUModelConfig
-from rasa_nlu.model import Trainer
 from rasa_nlu import config
-from rasa_nlu.model import Metadata, Interpreter
-import json
+from rasa_nlu.model import Interpreter
+from rasa_nlu.model import Trainer
+from rasa_nlu.training_data import load_data
+
 
 def train (data, config_file, model_dir):
     training_data = load_data(data)
     trainer = Trainer(config.load(config_file))
-    trainer.train(training_data)
-    trainer.persist(model_dir, fixed_model_name = 'chat')
+    trainer.train(training_data,num_threads=3)
+    trainer.persist(model_dir, fixed_model_name='chat')
 
-train('nlu.md', 'nlu_config.yml', 'models/nlu')
+train('data/nlu.md', 'nlu_config.yml', 'models/nlu')
 interpreter = Interpreter.load('./models/nlu/default/chat')
 
 # define function to ask question
 def ask_question(text):
     print(interpreter.parse(text))
 
-ask_question('em sinh ngày bao nhiêu')
-ask_question('cho anh số điện thoại đi')
+ask_question('hello em')
+ask_question('hi')
